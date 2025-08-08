2050759040397c98198f42af2ab71db457831567
Gabriela B. Correa <86533992+gabriela-correa@users.noreply.github.com>
2025-08-08 01:03:14 -0300

    Add files via upload

---
+++ MD_Simulations/system 1: CO2-nC7/298K-10bar/trans-prop/diffusivity.py
@@ -0,0 +1,69 @@
+
+"""
+
+SELF-DIFFUSIVITY
+
+
+"""
+import numpy as np
+from numba import njit
+import mmap
+
+
+""" INPUTS """
+freq = 200 # data output frequency
+ts = 1 # fs - timestep
+nsteps_total = 10000000 # total number of steps
+nsteps_amostra = 100000 
+norig = 100 
+N1 = 10 #number of co2 molecules
+
+""" INITIAL CALCULATIONS """
+dt = freq*ts 
+nframes = int(nsteps_total/freq) 
+nlen = int(nsteps_amostra/freq + 1)  
+n_init = int(np.floor((nsteps_total-nsteps_amostra)/(norig*freq)) + 1)  
+
+""" STARTING MATRICES AND VECTORS """
+r1_0 = np.zeros((N1, 3), dtype=np.float64) 
+r1 = np.zeros((nframes, N1, 3), dtype=np.float64) 
+BSD1 = np.zeros((nlen), dtype=np.float64) 
+
+""" READ CENTER OF MASS COORDINATES """
+with open("1.out", "r") as f:
+    mmapped_file = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    linha_inicio = 0
+    for _ in range(3):
+        linha_inicio = mmapped_file.find(b'\n', linha_inicio) + 1
+    for i in range(nframes):
+        linha_inicio = mmapped_file.find(b'\n', linha_inicio) + 1
+        for j in range(N1):
+            linha_fim = mmapped_file.find(b'\n', linha_inicio)
+            linha = mmapped_file[linha_inicio:linha_fim].decode('utf-8').split()
+            n, x, y, z = map(float, linha) 
+            r1[i,j,:] = x, y, z          
+            linha_inicio = linha_fim + 1
+        
+""" MAIN """
+@njit
+def calculos(r1, BSD1, nframes, norig, nlen, n_init, N1):
+    nfun = 0
+    for i in range(n_init): 
+        run = int(np.floor( (nframes - i*norig)/nlen  )) 
+        for j in range(run):
+            nfun = nfun + 1
+            frame0 = int(i*norig + j*nlen)
+            r1_0 = np.copy(r1[frame0,:,:])
+            for m in range(nlen):
+                frame = int(frame0 + m)
+                BSD1[m] = BSD1[m] + np.sum( (r1[frame,:,:]-r1_0)**2 )/N1
+
+    return BSD1, nfun
+    
+BSD1, nfun = calculos(r1, BSD1, nframes, norig, nlen, n_init, N1)
+
+BSD1 = BSD1/nfun
+
+np.savetxt('data_MSD_1.txt', BSD1, fmt='%e')
+
+
---
+++ MD_Simulations/system 1: CO2-nC7/298K-10bar/trans-prop/run.py
@@ -0,0 +1,45 @@
+import numpy as np
+from random import *
+import os
+
+T = 298.0
+a = 1
+b = 10
+c = 11
+d = 7010
+
+pwd=os.getcwd()
+
+for i in range(1,31):
+ 
+    os.system(f"mkdir {int(i)}")
+    os.system("cp in.run "+pwd+f"/{int(i)}/in.run")
+    os.system("cp viscosity.py "+pwd+f"/{int(i)}/viscosity.py")
+    os.system("cp diffusivity.py "+pwd+f"/{int(i)}/diffusivity.py")    
+    os.system("cp sub.lmp "+pwd+f"/{int(i)}/sub.lmp") 
+     
+    os.chdir(pwd+f"/{int(i)}")
+    with open('in.run', 'r') as file :
+        filedata = file.read()
+    file_fixed1 = filedata.replace('xst1', f'{T}')
+    file_fixed2 = file_fixed1.replace('xst2', f'{randint(0,100000)}')
+    file_fixed3 = file_fixed2.replace('xsta', f'{int(a)}')
+    file_fixed4 = file_fixed3.replace('xstb', f'{int(b)}')
+    file_fixed5 = file_fixed4.replace('xstc', f'{int(c)}')
+    file_fixed6 = file_fixed5.replace('xstd', f'{int(d)}')
+    with open('in.run', 'w') as file:
+        file.write(file_fixed6)  
+    os.chdir(pwd)
+
+
+    
+    
+    
+    
+    
+    
+
+
+
+
+
