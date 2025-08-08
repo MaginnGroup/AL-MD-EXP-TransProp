0442af74b2a137b150c0ae32ef3075af5f0f84e7
Gabriela B. Correa <86533992+gabriela-correa@users.noreply.github.com>
2025-08-08 00:47:25 -0300

    Add files via upload

---
+++ Active_Learning/system 2: CO2-benzene/EXP-versus-MD/plot.py
@@ -0,0 +1,151 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import figstyle
+
+
+fig, axs = plt.subplots(2, 2, figsize=(5, 4), dpi=300)
+
+data = np.load("../single-fidelity-EXP/data_Diffusivity_EXP.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+errY_s = data["errY_s"]
+axs[0,0].errorbar(X_s[0][:,1], np.squeeze(Y_s[0]), np.squeeze(errY_s[0]), fmt='o', color='black', label='100 bar')
+axs[0,0].errorbar(X_s[1][:,1], np.squeeze(Y_s[1]), np.squeeze(errY_s[1]), fmt='>', color='blue', label='110 bar')
+axs[0,0].errorbar(X_s[2][:,1], np.squeeze(Y_s[2]), np.squeeze(errY_s[2]), fmt='*', color='red', label='130 bar')
+axs[0,0].errorbar(X_s[3][:,1], np.squeeze(Y_s[3]), np.squeeze(errY_s[3]), fmt='D', color='purple', label='150 bar')
+axs[0,0].errorbar(X_s[4][:,1], np.squeeze(Y_s[4]), np.squeeze(errY_s[4]), fmt='^', color='green', label='200 bar')
+axs[0,0].errorbar(X_s[5][:,1], np.squeeze(Y_s[5]), np.squeeze(errY_s[5]), fmt='o', color='m', label='300 bar')
+
+data = np.load("../single-fidelity-EXP/data_Diffusivity_EXP_gp.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+STD_s = data["STD_s"]
+axs[0,0].plot(X_s[0][:,1], Y_s[0], color='black')
+axs[0,0].plot(X_s[1][:,1], Y_s[1], color='blue')
+axs[0,0].plot(X_s[2][:,1], Y_s[2], color='red')
+axs[0,0].plot(X_s[3][:,1], Y_s[3], color='purple')
+axs[0,0].plot(X_s[4][:,1], Y_s[4], color='green')
+axs[0,0].plot(X_s[5][:,1], Y_s[5], color='m')
+
+Y_exp_D = data["Y_s"]
+
+
+data = np.load("../single-fidelity-EXP/data_Viscosity_EXP.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+errY_s = data["errY_s"]
+axs[1,0].errorbar(X_s[0][:,1], np.squeeze(Y_s[0]), np.squeeze(errY_s[0]), fmt='o', color='black', label='100 bar')
+axs[1,0].errorbar(X_s[1][:,1], np.squeeze(Y_s[1]), np.squeeze(errY_s[1]), fmt='>', color='blue', label='110 bar')
+axs[1,0].errorbar(X_s[2][:,1], np.squeeze(Y_s[2]), np.squeeze(errY_s[2]), fmt='*', color='red', label='130 bar')
+axs[1,0].errorbar(X_s[3][:,1], np.squeeze(Y_s[3]), np.squeeze(errY_s[3]), fmt='D', color='purple', label='150 bar')
+axs[1,0].errorbar(X_s[4][:,1], np.squeeze(Y_s[4]), np.squeeze(errY_s[4]), fmt='^', color='green', label='200 bar')
+axs[1,0].errorbar(X_s[5][:,1], np.squeeze(Y_s[5]), np.squeeze(errY_s[5]), fmt='o', color='m', label='300 bar')
+
+
+data = np.load("../single-fidelity-EXP/data_Viscosity_EXP_gp.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+STD_s = data["STD_s"]
+axs[1,0].plot(X_s[0][:,1], Y_s[0], color='black')
+axs[1,0].plot(X_s[1][:,1], Y_s[1], color='blue')
+axs[1,0].plot(X_s[2][:,1], Y_s[2], color='red')
+axs[1,0].plot(X_s[3][:,1], Y_s[3], color='purple')
+axs[1,0].plot(X_s[4][:,1], Y_s[4], color='green')
+axs[1,0].plot(X_s[5][:,1], Y_s[5], color='m')
+
+Y_exp_V = data["Y_s"]
+
+
+data = np.load("../single-fidelity-MD/data_Diffusivity_MD.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+errY_s = data["errY_s"]
+axs[0,1].errorbar(X_s[0][:,1], np.squeeze(Y_s[0]), np.squeeze(errY_s[0]), fmt='o', color='black', label='100 bar')
+axs[0,1].errorbar(X_s[1][:,1], np.squeeze(Y_s[1]), np.squeeze(errY_s[1]), fmt='>', color='blue', label='110 bar')
+axs[0,1].errorbar(X_s[2][:,1], np.squeeze(Y_s[2]), np.squeeze(errY_s[2]), fmt='*', color='red', label='130 bar')
+axs[0,1].errorbar(X_s[3][:,1], np.squeeze(Y_s[3]), np.squeeze(errY_s[3]), fmt='D', color='purple', label='150 bar')
+axs[0,1].errorbar(X_s[4][:,1], np.squeeze(Y_s[4]), np.squeeze(errY_s[4]), fmt='^', color='green', label='200 bar')
+axs[0,1].errorbar(X_s[5][:,1], np.squeeze(Y_s[5]), np.squeeze(errY_s[5]), fmt='o', color='m', label='300 bar')
+
+
+data = np.load("../single-fidelity-MD/data_Diffusivity_MD_gp.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+STD_s = data["STD_s"]
+axs[0,1].plot(X_s[0][:,1], Y_s[0], color='black')
+axs[0,1].plot(X_s[1][:,1], Y_s[1], color='blue')
+axs[0,1].plot(X_s[2][:,1], Y_s[2], color='red')
+axs[0,1].plot(X_s[3][:,1], Y_s[3], color='purple')
+axs[0,1].plot(X_s[4][:,1], Y_s[4], color='green')
+axs[0,1].plot(X_s[5][:,1], Y_s[5], color='m')
+
+Y_sim_D = data["Y_s"]
+
+
+data = np.load("../single-fidelity-MD/data_Viscosity_MD.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+errY_s = data["errY_s"]
+axs[1,1].errorbar(X_s[0][:,1], np.squeeze(Y_s[0]), np.squeeze(errY_s[0]), fmt='o', color='black', label='100 bar')
+axs[1,1].errorbar(X_s[1][:,1], np.squeeze(Y_s[1]), np.squeeze(errY_s[1]), fmt='>', color='blue', label='110 bar')
+axs[1,1].errorbar(X_s[2][:,1], np.squeeze(Y_s[2]), np.squeeze(errY_s[2]), fmt='*', color='red', label='130 bar')
+axs[1,1].errorbar(X_s[3][:,1], np.squeeze(Y_s[3]), np.squeeze(errY_s[3]), fmt='D', color='purple', label='150 bar')
+axs[1,1].errorbar(X_s[4][:,1], np.squeeze(Y_s[4]), np.squeeze(errY_s[4]), fmt='^', color='green', label='200 bar')
+axs[1,1].errorbar(X_s[5][:,1], np.squeeze(Y_s[5]), np.squeeze(errY_s[5]), fmt='o', color='m', label='300 bar')
+
+
+data = np.load("../single-fidelity-MD/data_Viscosity_MD_gp.npz", allow_pickle=True)
+X_s = data["X_s"]
+Y_s = data["Y_s"]
+STD_s = data["STD_s"]
+axs[1,1].plot(X_s[0][:,1], Y_s[0], color='black')
+axs[1,1].plot(X_s[1][:,1], Y_s[1], color='blue')
+axs[1,1].plot(X_s[2][:,1], Y_s[2], color='red')
+axs[1,1].plot(X_s[3][:,1], Y_s[3], color='purple')
+axs[1,1].plot(X_s[4][:,1], Y_s[4], color='green')
+axs[1,1].plot(X_s[5][:,1], Y_s[5], color='m')
+
+Y_sim_V = data["Y_s"]
+
+from matplotlib.lines import Line2D
+legend_elements = [
+    Line2D([0], [0], marker='o', color='black', label='100 bar'),
+    Line2D([0], [0], marker='>', color='blue', label='110 bar'),
+    Line2D([0], [0], marker='*', color='red', label='130 bar'),
+    Line2D([0], [0], marker='D', color='purple', label='150 bar'),
+    Line2D([0], [0], marker='^', color='green', label='200 bar'),
+    Line2D([0], [0], marker='o', color='m', label='300 bar')
+]
+
+axs[0,0].legend(handles=legend_elements, ncol=2, fontsize=7, handletextpad=0.4, labelspacing=0.3, handlelength=1.2) 
+
+axs[0,0].set_title("Experimental Data")
+axs[0,1].set_title("Simulated Data")
+axs[0,0].set_ylabel(r'$D \times 10^{-9}$ m²/s')
+axs[1,0].set_ylabel(r'$\eta \times 10^{-5}$ Pa s')
+axs[1,0].set_xlabel(r'$T$ (K)')
+axs[1,1].set_xlabel(r'$T$ (K)')
+axs[0,0].set_xticks([300,320,340,360,380])
+axs[0,1].set_xticks([300,320,340,360,380])
+axs[1,0].set_xticks([300,320,340,360,380])
+axs[1,1].set_xticks([300,320,340,360,380])
+axs[0,1].set_yticklabels([])
+axs[0,0].set_xticklabels([])
+axs[0,1].set_xticklabels([])
+axs[1,1].set_yticklabels([])
+axs[0,0].set_ylim(5,80)
+axs[0,1].set_ylim(5,80)
+axs[1,0].set_ylim(1,11)
+axs[1,1].set_ylim(1,11)
+plt.tight_layout()
+plt.savefig('figure1.png', dpi=600) 
+
+
+MRE1 = (100*np.abs(Y_sim_D-Y_exp_D)/Y_exp_D)
+MRE2 = (100*np.abs(Y_sim_V-Y_exp_V)/Y_exp_V)
+print(f"MRE Diffusivity: min {MRE1.min():.2f}%, max {MRE1.max():.2f}%, mean {MRE1.mean():.2f}%")
+print(f"MRE Viscosity: min {MRE2.min():.2f}%, max {MRE2.max():.2f}%, mean {MRE2.mean():.2f}%")
+
+
+
+
---
+++ Active_Learning/system 2: CO2-benzene/single-fidelity-EXP/Viscosity_SingleGP_Training.py
@@ -0,0 +1,213 @@
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+import os
+import warnings
+import numpy
+import pandas
+from sklearn import preprocessing
+from matplotlib import pyplot as plt
+
+import torch
+from botorch.models import SingleTaskGP
+from botorch.fit import fit_gpytorch_mll
+from gpytorch.likelihoods import FixedNoiseGaussianLikelihood
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from gpytorch.kernels import RBFKernel
+
+import figstyle
+
+# =============================================================================
+# Configuration
+# =============================================================================
+
+# Path to database folder
+dbPath=r'../Databases'
+# Database code
+code='BENZ_SCO2_EXP'
+# Define normalization methods
+featureNorm='MinMax' # Standardization,LogStand,MinMax
+labelNorm='LogStand' # Standardization,LogStand,MinMax
+
+# =============================================================================
+# Auxiliary Functions
+# =============================================================================
+
+def normalize(inputArray,skScaler=None,method='Standardization',reverse=False):
+    # If inputArray is a labels vector of size (N,), reshape to (N,1)
+    if inputArray.ndim==1:
+        inputArray=inputArray.reshape((-1,1))
+        warnings.warn('Input to normalize() was of shape (N,). It was assumed'\
+                      +' to be a column array and converted to a (N,1) shape.')
+    # If skScaler is None, train for the first time
+    if skScaler is None:
+        # Check method
+        if method=='Standardization' or method=='MinMax': aux=inputArray
+        elif method=='LogStand' or method=='Log': aux=numpy.log(inputArray)
+        else: raise ValueError('Could not recognize method in normalize().')
+        if method=='MinMax':
+            skScaler=preprocessing.MinMaxScaler().fit(aux)
+        elif method=='Log':
+            skScaler='NA'
+        else:
+            skScaler=preprocessing.StandardScaler().fit(aux) 
+    # Do main operation (normalize or unnormalize)
+    if reverse:
+        # Rescale the data back to its original distribution
+        if method!='Log':
+            inputArray=skScaler.inverse_transform(inputArray)
+        # Check method
+        if method=='LogStand' or method=='Log':
+            inputArray=numpy.exp(inputArray)
+    elif not reverse:
+        # Check method
+        if method=='Standardization' or method=='MinMax': aux=inputArray
+        elif method=='LogStand' or method=='Log': aux=numpy.log(inputArray)
+        else: raise ValueError('Could not recognize method in normalize().')
+        if method!='Log':
+            inputArray=skScaler.transform(aux)
+        else:
+            inputArray=aux
+    # Return
+    return inputArray,skScaler
+
+# =============================================================================
+# Load Data
+# =============================================================================
+
+trainDB_Path=os.path.join(dbPath,code+'.csv')
+trainDB=pandas.read_csv(trainDB_Path)
+
+# =============================================================================
+# Training
+# =============================================================================
+
+# Defining the input/output vectors
+X=numpy.array([trainDB['P/bar'].values,trainDB['T/K'].values]).T
+Y=numpy.array([trainDB['Visc/Pas'].values]).T *1e5
+errY = 0.02 * Y
+X_Train = X.copy()
+Y_Train = Y.copy()
+
+# Normalize
+X_Train_N,skScaler_X=normalize(X_Train,method=featureNorm)
+Y_Train_N,skScaler_Y=normalize(Y_Train,method=labelNorm)
+errY_Train_N = errY / Y / skScaler_Y.scale_
+
+# Torch tensors
+train_X = torch.tensor(X_Train_N, dtype=torch.float64)
+train_Y = torch.tensor(Y_Train_N, dtype=torch.float64)
+train_errY = torch.tensor(errY_Train_N, dtype=torch.float64)
+train_Yvar = (train_errY**2).flatten().to(train_Y.dtype)
+
+# Build GP model
+likelihood = FixedNoiseGaussianLikelihood(noise=train_Yvar, learn_additional_noise=False)
+model = SingleTaskGP(train_X, train_Y, likelihood=likelihood, covar_module = RBFKernel())
+mll = ExactMarginalLogLikelihood(model.likelihood, model)
+fit_gpytorch_mll(mll)
+
+# Predictions
+model.eval()
+with torch.no_grad():
+    pred = model.posterior(train_X)
+    mean = pred.mean.numpy()
+    var = pred.variance.numpy()
+    std = numpy.sqrt(var)
+
+# Unnormalize prediction
+Y_pred, _ = normalize(mean, method=labelNorm, skScaler=skScaler_Y, reverse=True)
+
+# Compute MRE
+mre = 100 * numpy.abs(Y_pred - Y) / Y
+print(f"Mean Relative Error (MRE): {mre.mean():.2f}%")
+
+# =============================================================================
+# Generate Model Data
+# =============================================================================
+
+pres = numpy.array([100,110,130,150,200,300])
+temp = numpy.arange(303, 374, 1)
+X_new = numpy.array([[P, T] for T in temp for P in pres])
+X_new_N,skScaler_X=normalize(X_new,method=featureNorm)
+X_new_tensor=torch.tensor(X_new_N, dtype=torch.float64)
+
+with torch.no_grad():
+    pred = model.posterior(X_new_tensor)
+    mean = pred.mean.numpy()
+    std = numpy.sqrt(pred.variance.numpy())
+
+Y_new, _ = normalize(mean, method=labelNorm, skScaler=skScaler_Y, reverse=True)
+
+# LogStand variance transform
+#mu_log = mean * skScaler_Y.scale_ + skScaler_Y.mean_
+#sigma_log = std * skScaler_Y.scale_
+#STD_new = numpy.sqrt(   (numpy.exp(sigma_log**2) - 1) * numpy.exp(2 * mu_log + sigma_log**2) )
+STD_new = std * skScaler_Y.scale_ * Y_new
+
+AF = 100 * STD_new / Y_new
+print(f"AF (max): {AF.max():.2f}%")
+
+# =============================================================================
+# Plots
+# =============================================================================
+
+fig = plt.figure(figsize=(3.0, 2.5), dpi=300)
+
+temp1 = numpy.unique(X[:, 0])
+X_s = []
+Y_s = []
+errY_s = []
+for temp in temp1:
+    indices = numpy.where(X[:, 0] == temp)[0]
+    X_sub = X[indices]
+    Y_sub = Y[indices]  
+    errY_sub = errY[indices]
+    X_s.append(X_sub)
+    Y_s.append(Y_sub)
+    errY_s.append(errY_sub)
+plt.errorbar(X_s[0][:,1], numpy.squeeze(Y_s[0]), numpy.squeeze(errY_s[0]), fmt='o', color='black')
+plt.errorbar(X_s[1][:,1], numpy.squeeze(Y_s[1]), numpy.squeeze(errY_s[1]), fmt='>', color='blue')
+plt.errorbar(X_s[2][:,1], numpy.squeeze(Y_s[2]), numpy.squeeze(errY_s[2]), fmt='*', color='red')
+plt.errorbar(X_s[3][:,1], numpy.squeeze(Y_s[3]), numpy.squeeze(errY_s[3]), fmt='D', color='pink')
+plt.errorbar(X_s[4][:,1], numpy.squeeze(Y_s[4]), numpy.squeeze(errY_s[4]), fmt='<', color='green')
+plt.errorbar(X_s[5][:,1], numpy.squeeze(Y_s[5]), numpy.squeeze(errY_s[5]), fmt='d', color='m')
+
+numpy.savez("data_Viscosity_EXP.npz", X_s=X_s, Y_s=Y_s, errY_s=errY_s)
+
+temp1 = numpy.unique(X_new[:, 0])
+X_s = []
+Y_s = []
+STD_s = []
+for temp in temp1:
+    indices = numpy.where(X_new[:, 0] == temp)[0]
+    X_sub = X_new[indices]
+    Y_sub = Y_new[indices]   
+    STD_sub = STD_new[indices] 
+    X_s.append(X_sub)
+    Y_s.append(Y_sub)
+    STD_s.append(STD_sub)
+plt.plot(X_s[0][:,1], Y_s[0], color='black')
+plt.plot(X_s[1][:,1], Y_s[1], color='blue')
+plt.plot(X_s[2][:,1], Y_s[2], color='red')
+plt.plot(X_s[3][:,1], Y_s[3], color='pink')
+plt.plot(X_s[4][:,1], Y_s[4], color='green')
+plt.plot(X_s[5][:,1], Y_s[5], color='m')
+plt.fill_between(X_s[0][:,1].flatten(), (Y_s[0] - 2*STD_s[0]).flatten(), (Y_s[0] + 2*STD_s[0]).flatten(), color='black', alpha=0.2)
+plt.fill_between(X_s[1][:,1].flatten(), (Y_s[1] - 2*STD_s[1]).flatten(), (Y_s[1] + 2*STD_s[1]).flatten(), color='blue', alpha=0.2)
+plt.fill_between(X_s[2][:,1].flatten(), (Y_s[2] - 2*STD_s[2]).flatten(), (Y_s[2] + 2*STD_s[2]).flatten(), color='red', alpha=0.2)
+plt.fill_between(X_s[3][:,1].flatten(), (Y_s[3] - 2*STD_s[3]).flatten(), (Y_s[3] + 2*STD_s[3]).flatten(), color='pink', alpha=0.2)
+plt.fill_between(X_s[4][:,1].flatten(), (Y_s[4] - 2*STD_s[4]).flatten(), (Y_s[4] + 2*STD_s[4]).flatten(), color='green', alpha=0.2)
+plt.fill_between(X_s[5][:,1].flatten(), (Y_s[5] - 2*STD_s[5]).flatten(), (Y_s[5] + 2*STD_s[5]).flatten(), color='m', alpha=0.2)
+
+numpy.savez("data_Viscosity_EXP_gp.npz", X_s=X_s, Y_s=Y_s, STD_s=STD_s)
+
+plt.ylabel(r'$\eta \times 10^{-5}$ Pa s')
+plt.xlabel(r'$T$ (K)')
+plt.xticks([300,320,340,360,380])
+plt.ylim(1,11)
+plt.tight_layout()
+#plt.savefig('Training_Viscosity.png', dpi=600) 
+
+
